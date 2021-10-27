98f81627ff63c6918cd5cad2de52f8d11290fc1a
Sergey <21218903+xchakiex@users.noreply.github.com>
2021-10-27 16:32:24 +0300

    colors state + simple button added

---
+++ TrafficLightsSwiftUI/ColorCircleView.swift
@@ -0,0 +1,28 @@
+//
+//  ColorCircleView.swift
+//  TrafficLightsSwiftUI
+//
+//  Created by Nechaev Sergey  on 27.10.2021.
+//
+
+import SwiftUI
+
+struct ColorCircleView: View {
+    let color: Color
+    var opacity: Double
+    
+    var body: some View {
+        Circle()
+            .foregroundColor(color.opacity(opacity))
+            .frame(width: 100, height: 100)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct ColorCircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorCircleView(color: .blue, opacity: 1.0)
+    }
+}
--- TrafficLightsSwiftUI/ContentView.swift
+++ TrafficLightsSwiftUI/ContentView.swift
@@ -7,11 +7,66 @@
 
 import SwiftUI
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 struct ContentView: View {
+    private let lightIsOn = 1.0
+    private let lightIsOff = 0.3
+    
+    @State private var currentLight = CurrentLight.red
+    
+    @State private var redLightOpacity = 0.3
+    @State private var yellowLightOpacity = 0.3
+    @State private var greenLightOpacity = 0.3
+    
+    @State private var buttonText = "START"
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            VStack {
+                ColorCircleView(color: .red, opacity: redLightOpacity)
+                    .padding(.bottom)
+                ColorCircleView(color: .yellow, opacity: yellowLightOpacity)
+                    .padding(.bottom)
+                ColorCircleView(color: .green, opacity: greenLightOpacity)
+                    .padding(.bottom)
+                
+                Spacer()
+                
+                Button(action: {
+                    if buttonText == "START" {
+                        buttonText = "NEXT"
+                    }
+                    switch currentLight {
+                    case .red:
+                        redLightOpacity = lightIsOn
+                        yellowLightOpacity = lightIsOff
+                        greenLightOpacity = lightIsOff
+                        currentLight = .yellow
+                    case .yellow:
+                        redLightOpacity = lightIsOff
+                        yellowLightOpacity = lightIsOn
+                        greenLightOpacity = lightIsOff
+                        currentLight = .green
+                    case .green:
+                        redLightOpacity = lightIsOff
+                        yellowLightOpacity = lightIsOff
+                        greenLightOpacity = lightIsOn
+                        currentLight = .red
+                    }
+                }) {
+                    Text(buttonText)
+                        .font(.title)
+                }
+            }
             .padding()
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +74,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
